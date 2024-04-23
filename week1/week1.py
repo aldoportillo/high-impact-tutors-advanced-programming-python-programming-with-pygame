200719ac45828d2938fbcc4d313c4a6ff636a84f
Carman Chiu <101212528+carmanchiu@users.noreply.github.com>
2024-04-23 16:19:39 -0500

    jotted quick notes for next week's lesson plan

--- week1/week1.py
+++ week1/week1.py
@@ -1,2 +1,11 @@
+# Functions
+def circle_shaded_area(radius_outside, radius_inside):
+    area_outside = 3.14 * radius_outside ** 2
+    area_inside = 3.14 * radius_inside ** 2
+    area_shaded = area_outside - area_inside
+    return area_shaded
+
 # Using logical operators to test functions
 answer_is_incorrect = (area_shaded > area_outside) || (area_shaded < area_inside)
+
+# Program for student's fundraising event?
